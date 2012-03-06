cd220ff86240f6ac73e049349a113ece6c7bc01f
Dmitry <DmitryRodov@ya.ru>
2012-03-07 00:21:33 +0300

    1) View of all processes and management

---
+++ TaskManager/TaskManager/CreatProcWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for CreatProcWindow.xaml
+    /// </summary>
+    public partial class CreatProcWindow : Window
+    {
+        public CreatProcWindow()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(textBox1.Text);
+                this.Close();
+            }
+            catch (Exception excpt)
+            {
+                textBox1.Text = excpt.Message;
+            }
+        }
+    }
+}
---
+++ TaskManager/TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListViewItem temp = new ListViewItem();
+            ObservableCollection<Process> ProcColl = new ObservableCollection<Process>();
+            foreach (Process proc in Process.GetProcesses())
+            {
+                ProcColl.Add(proc);
+            }
+            ProcListView.ItemsSource = ProcColl;
+        }
+
+        private void ProcRefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListViewItem temp = new ListViewItem();
+            ObservableCollection<Process> ProcColl = new ObservableCollection<Process>();
+            foreach (Process proc in Process.GetProcesses())
+            {
+                ProcColl.Add(proc);
+            }
+            ProcListView.ItemsSource = ProcColl;
+        }
+
+        private void ProcCreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreatProcWindow CrPrWind = new CreatProcWindow();
+            CrPrWind.ShowDialog();
+            ProcRefreshButton_Click(sender, e);
+        }
+
+        private void ProcKillButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process ProcToKill = (Process)ProcListView.SelectedItem;
+                if (ProcToKill == null)
+                    return;
+                ProcToKill.Kill();
+                ProcRefreshButton_Click(sender, e);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
