81bcf472681f03e428f51d765c474acb769e0602
Dmitry <DmitryRodov@ya.ru>
2012-03-08 01:18:50 +0300

    2) Tasks and processes Without: - info about mem, CPU - additional info about proc

--- TaskManager/TaskManager/MainWindow.xaml.cs
+++ TaskManager/TaskManager/MainWindow.xaml.cs
@@ -21,34 +21,69 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Process> ProcColl;
+        List<Process> AppColl;
+        int FlagProcSort = 0;
+        int FlagAppSort = 0;
         public MainWindow()
         {
             InitializeComponent();
-  //          ListViewItem temp = new ListViewItem();
-            List<Process> ProcColl = new List<Process>();
+            ProcRefresh();
+            AppRefresh();
+
+        }
+
+        void ProcRefresh()
+        {
+            ProcColl = new List<Process>();
             foreach (Process proc in Process.GetProcesses())
             {
                 ProcColl.Add(proc);
             }
+            ProcCountLabel.Content = ProcColl.Count.ToString();
+            switch (FlagProcSort)
+            {
+                case 1: SortByName(ProcColl); break;
+                case 2: SortById(ProcColl); break;
+                case 3: SortByThreads(ProcColl); break;
+                default: break;
+            }
             ProcListView.ItemsSource = ProcColl;
+        }
+
+        void AppRefresh()
+        {
+            AppColl = new List<Process>();
+            foreach (Process proc in Process.GetProcesses())
+            {
+                if(proc.MainWindowTitle != "")
+                    AppColl.Add(proc);
+            }
+            switch (FlagAppSort)
+            {
+                case 1: SortByTask(AppColl); break;
+                case 2: SortByResp(AppColl); break;
+                default: break;
+            }
+            AppListView.ItemsSource = AppColl;
         }
 
         private void ProcRefreshButton_Click(object sender, RoutedEventArgs e)
         {
- //           ListViewItem temp = new ListViewItem();
-            List<Process> ProcColl = new List<Process>();
-            foreach (Process proc in Process.GetProcesses())
-            {
-                ProcColl.Add(proc);
-            }
-            ProcListView.ItemsSource = ProcColl;
+            ProcRefresh();
         }
 
-        private void ProcCreateButton_Click(object sender, RoutedEventArgs e)
+        private void AppRefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppRefresh();
+        }
+
+        private void TaskStartButton_Click(object sender, RoutedEventArgs e)
         {
             CreatProcWindow CrPrWind = new CreatProcWindow();
             CrPrWind.ShowDialog();
-            ProcRefreshButton_Click(sender, e);
+            AppRefresh();
+            ProcRefresh();
         }
 
         private void ProcKillButton_Click(object sender, RoutedEventArgs e)
@@ -59,7 +94,22 @@ namespace WpfApplication1
                 if (ProcToKill == null)
                     return;
                 ProcToKill.Kill();
-                ProcRefreshButton_Click(sender, e);
+                ProcRefresh();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void AppCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process ProcToClose = (Process)AppListView.SelectedItem;
+                if (ProcToClose == null)
+                    return;
+                ProcToClose.CloseMainWindow();
+                AppRefresh();
             }
             catch (Exception)
             {
@@ -71,19 +121,82 @@ namespace WpfApplication1
 
         }
 
-        private void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+        private void GridViewProcColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
         {
-            List<Process> ProcColl = new List<Process>(ProcListView.ItemsSource as List<Process>);
-            ProcColl.Sort(new Comparison<Process>(delegate( Process a, Process b)
+            switch ((e.OriginalSource as GridViewColumnHeader).Content.ToString().ToLower())
+            {
+                case "name": FlagProcSort = 1; break;
+                case "id": FlagProcSort = 2; break;
+                case "threads": FlagProcSort = 3; break;
+                default: FlagProcSort = 0; break;
+            }
+            ProcRefresh();
+        }
+
+        private void GridViewAppColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+        {
+            switch ((e.OriginalSource as GridViewColumnHeader).Content.ToString().ToLower())
+            {
+                case "task": FlagAppSort = 1; break;
+                case "responding": FlagAppSort = 2; break;
+                default: FlagAppSort = 0; break;
+            }
+            AppRefresh();
+        }
+
+        void SortByName(List<Process> lst)
+        {
+            lst.Sort(new Comparison<Process>(delegate(Process a, Process b)
             {
                 return String.Compare(a.ProcessName, b.ProcessName);
             }));
-            ProcListView.ItemsSource = ProcColl;
-            /*switch ((e.OriginalSource as GridViewColumnHeader).Content.ToString())
-            {
-                case "Name": ProcColl.ToArray
-            }*/
+        }
 
+        void SortById(List<Process> lst)
+        {
+            lst.Sort(new Comparison<Process>(delegate(Process a, Process b)
+            {
+                if (a.Id > b.Id)
+                    return 1;
+                else if (a.Id == b.Id)
+                    return 0;
+                else
+                    return -1;
+            }));
+        }
+
+        void SortByThreads(List<Process> lst)
+        {
+            lst.Sort(new Comparison<Process>(delegate(Process a, Process b)
+            {
+                if (a.Threads.Count > b.Threads.Count)
+                    return 1;
+                else if (a.Threads.Count == b.Threads.Count)
+                    return 0;
+                else
+                    return -1;
+            }));
+        }
+
+        void SortByTask(List<Process> lst)
+        {
+            lst.Sort(new Comparison<Process>(delegate(Process a, Process b)
+            {
+                return String.Compare(a.MainWindowTitle.ToString(), b.MainWindowTitle.ToString());
+            }));
+        }
+
+        void SortByResp(List<Process> lst)
+        {
+            lst.Sort(new Comparison<Process>(delegate(Process a, Process b)
+            {
+                if (a.Responding == b.Responding)
+                    return 0;
+                else if (a.Responding == false && b.Responding == true)
+                    return 1;
+                else 
+                    return -1;
+            }));
         }
     }
 }
